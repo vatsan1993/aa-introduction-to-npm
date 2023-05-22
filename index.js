5efdcb41c217762da4eab9be93b5cc4d18c86278
Srivatsan <vatsan1993@gmail.com>
2023-05-22 21:24:47 +0530

    finished part 1

---
+++ index.js
@@ -0,0 +1,7 @@
+const colors = require('colors');
+
+console.log('hello'.green);
+console.log('i like case and pies'.underline.red);
+console.log('inverse the color'.inverse);
+console.log('OMG Rainbows!'.rainbow);
+console.log('Run the trap'.trap);
